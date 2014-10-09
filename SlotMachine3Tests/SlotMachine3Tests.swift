e9abc170d2d4378ac29e8cad5da3e7bd607f6167
kuchta300 <kuchta.300@icloud.com>
2014-10-09 17:17:25 -0400

    Initial Commit

---
+++ SlotMachine3Tests/SlotMachine3Tests.swift
@@ -0,0 +1,36 @@
+//
+//  SlotMachine3Tests.swift
+//  SlotMachine3Tests
+//
+//  Created by David Kuchta on 10/9/14.
+//  Copyright (c) 2014 David Kuchta. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class SlotMachine3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
