84cabf282c7baaa2c95abdf129ef025d6f865a9d
kuchta300 <kuchta.300@icloud.com>
2014-10-10 13:50:34 -0400

    Created base for slot struct file

---
+++ slot.swift
@@ -0,0 +1,17 @@
+//
+//  slot.swift
+//  SlotMachine3
+//
+//  Created by David Kuchta on 10/10/14.
+//  Copyright (c) 2014 David Kuchta. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+struct Slot {
+    var value = 0
+    var image = UIImage(named: "Ace")
+    var isRed = true
+}
