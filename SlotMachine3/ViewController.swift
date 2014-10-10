1a2738d4c74d6f4ccd68ac21dc4e7d3ac7c95414
kuchta300 <kuchta.300@icloud.com>
2014-10-10 13:23:13 -0400

    Successful Setup of Second Container

--- SlotMachine3/ViewController.swift
+++ SlotMachine3/ViewController.swift
@@ -18,16 +18,23 @@ class ViewController: UIViewController {
     var titleLabel: UILabel!
     
     // Constants
+    
+    let kNumberOfContainers = 3
+    let kNumberOfSlots = 3
+
     let kMarginForView:CGFloat = 10.0
-  
+    let kMarginForSlot:CGFloat = 2.0
     
     let kSixth:CGFloat = 1.0/6.0
+    let kThird:CGFloat = 1.0/3.0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         setupContainerViews()
         setupFirstContainer(self.firstContainer)
+        setupSecondContainer(self.secondContainer)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -61,6 +68,20 @@ class ViewController: UIViewController {
         self.titleLabel.sizeToFit()
         self.titleLabel.center = containerView.center
         containerView.addSubview(self.titleLabel)
+    }
+    
+    func setupSecondContainer(containerView: UIView) {
+        
+        for var containerNumber = 0; containerNumber < kNumberOfContainers; ++containerNumber {
+            
+            for var slotNumber = 0; slotNumber < kNumberOfSlots; ++slotNumber {
+                
+                var slotImageView = UIImageView()
+                slotImageView.backgroundColor = UIColor.yellowColor()
+                slotImageView.frame = CGRect(x: containerView.bounds.origin.x + (containerView.bounds.size.width * CGFloat(containerNumber) * kThird), y: containerView.bounds.origin.y + (containerView.bounds.size.height * CGFloat(slotNumber) * kThird), width: containerView.bounds.width * kThird - kMarginForSlot, height: containerView.bounds.height * kThird - kMarginForSlot)
+                containerView.addSubview(slotImageView)
+            }
+        }
     }
     
 }// last brace
