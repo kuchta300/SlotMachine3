e436ef7cbfc5cb91645de8272b7bcbfc6d01960f
kuchta300 <kuchta.300@icloud.com>
2014-10-11 07:38:50 -0400

    Added in show alert with Text Function

--- SlotMachine3/ViewController.swift
+++ SlotMachine3/ViewController.swift
@@ -273,5 +273,12 @@ class ViewController: UIViewController {
         self.winnerPaidLabel.text = "\(winnings)"
     }
     
+    
+    func showAlertWithText (header : String = "Warning", message : String) {
+        var alert = UIAlertController(title: header, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
 }// last brace
 
