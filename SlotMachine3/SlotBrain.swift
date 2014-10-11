1eabffaa073ffbeb1c8ea117a2ef12dc4cdcbd7e
kuchta300 <kuchta.300@icloud.com>
2014-10-11 09:38:57 -0400

    Implemented Checks for flush
    Includes code for regular flush and royal flush

--- SlotMachine3/SlotBrain.swift
+++ SlotMachine3/SlotBrain.swift
@@ -41,9 +41,37 @@ class SlotBrain {
         var flushWinCount = 0
         var threeOfAKindWinCount = 0
         var straightWinCount = 0
+ 
         for slotRow in slotsInRows {
+            if checkFlush(slotRow) == true {
+                println("flush")
+                winnings += 1
+                flushWinCount += 1
+            }
         }
+ 
+        if flushWinCount == 3 {
+            println("Royal Flush")
+            winnings += 15
+        }
+
+        
         return winnings
     }
     
+    
+    class func checkFlush (slotRow: [Slot]) -> Bool {
+        let slot1 = slotRow[0]
+        let slot2 = slotRow[1]
+        let slot3 = slotRow[2]
+        if slot1.isRed == true && slot2.isRed == true && slot3.isRed == true {
+            return true
+        }
+        else if slot1.isRed == false && slot2.isRed == false && slot3.isRed == false {
+            return true
+        }
+        else {
+            return false
+        }
+    }
 }
